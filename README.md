b6de4cd043ae493129cdf3eaf5ffa3bc97be666e
Cezar Moise <git@cezarmoise.com>
2024-05-13 15:58:02 +0300

    Improve to allow negative selection of markers

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 Implementation of negative markers to allow excluding markers from tests that their class has set
 
-Only work for positive selections of markers, e.g. `-m marker` but not `-m "not marker"`
-
-
 List of tests
 - `test_class.py::TestClass_1` marked with `some_mark`
    - `test_class.py::TestClass_1::test_1_2`
@@ -49,9 +46,16 @@ test_class.py::TestClass_2::test_2_1
 Select all tests that don't have `some_mark`
 ```
 $ python -m pytest -q --collect-only -m "not some_mark"
+test_class.py::TestClass_1::test_1_3
 test_class.py::TestClass_2::test_2_1
 test_class.py::TestClass_2::test_2_2
 test_class.py::TestClass_2::test_2_3
 ```
 
-Notice that `test_class.py::TestClass_1::test_1_3` is not selected. This is becase our `pytest_collection_modifyitems` hook does not replace the original hook, it only runs before it. Our hook adds that test to the items list, but becase the parent `TestClass_1` has that marker, when the original hook runs, it will deselect it. The `item.iter_markers()` function return not just the current item's markers, but also those of the parent. A complete implementation would need to rewrite all the markers so that the markers of the parents are moved to the items themselves.
+Select all tests that don't have neither `some_mark` nor `some_mark2`
+```
+$ python -m pytest -q --collect-only -m "not some_mark and not some_mark2"
+test_class.py::TestClass_1::test_1_3
+test_class.py::TestClass_2::test_2_2
+test_class.py::TestClass_2::test_2_3
+```
--- conftest.py
+++ conftest.py
@@ -1,26 +1,30 @@
-import pytest
-from _pytest.mark import _parse_expression as parse_expression
+#!/usr/bin/env python3
 
-def pytest_collection_modifyitems(items, config):
-    matchexpr = config.option.markexpr
-    if not matchexpr:
-        return
+def pytest_collection_modifyitems(items):
+    """
+    Move the markers from the parents to the items
+    Remove markers that have an equivalent negative marker
 
-    expr = parse_expression(matchexpr, "Error with -m parameter")
-    remaining = []
-    deselected = []
+    before: 
+        item.own_markers = [mark_1, not_mark2]
+        item.parent.own_markers = [mark2, mark3]
+        item.iter_markers = [mark1, not_mark2, mark2, mark3]
+    after
+        item.own_markers = [mark_1, mark3]
+        item.parent.own_markers = []
+        item.iter_markers = [mark1, mark3]
+    """
 
     for item in items:
+        # iter_markers() return this item's markers + the markers of the parent
         markers = [m.name for m in item.iter_markers()]
         positive = set([m for m in markers if not m.startswith("not_")])
         negative = set([m[4:] for m in markers if m.startswith("not_")])
-        final = positive - negative
+        # only retain those markers that don't have an equivalent negative marker 
+        item.own_markers = [m for m in item.iter_markers() if m.name in positive - negative]
 
-        if expr.evaluate(lambda x: x in final):
-            remaining.append(item)
-        else:
-            deselected.append(item)
-
-    if deselected:
-        config.hook.pytest_deselected(items=deselected)
-        items[:] = remaining
+    for item in items:
+        for parent in item.iter_parents():
+            if item is not parent:
+                # remove markers from parents, so subsequent calls of this hook have the correct marker list
+                parent.own_markers = []
